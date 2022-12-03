d61820af8b89e8d87583770ca5e232d798172fbb
BauerLoser <hendrik.falk@t-online.de>
2022-12-03 12:21:51 +0100

    prediction in r

--- r/corona.R
+++ r/corona.R
@@ -1,6 +1,8 @@
 csv <- read.csv("..\\data\\Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv");
 csv_impfung <- read.csv("..\\data\\Aktuell_Deutschland_Impfquoten_COVID-19.csv");
 
+#final_data <- 0
+
 install_needed_package <- function(){
   install.packages("corrplot")
   install.packages("magrittr")
@@ -34,11 +36,13 @@ prepare_data <- function(){
   setDT(final_csv)
   setDT(impfugen_sum)
   
-  final_data <- merge(final_csv,impfugen_sum, by.x = "csv.Datum" ,by.y = "impfung.Impfdatum")
+  final_data <<- merge(final_csv,impfugen_sum, by.x = "csv.Datum" ,by.y = "impfung.Impfdatum")
   
   final_final_data <- data.frame(final_data$csv.X7T_Hospitalisierung_Faelle, final_data$csv.X7T_Hospitalisierung_Inzidenz,final_data$impfung.Anzahl)
   return(final_final_data)
 }
+
+
 
 plote_korrelationsmatrix <- function() {
   library(corrplot)
@@ -50,11 +54,18 @@ plote_korrelationsmatrix <- function() {
   csv_only_number_a <- filter(csv_only_number ,grepl('00\\+', csv_only_number$csv.Altersgruppe))
   
   csv_only_number_b <- data.frame(csv_only_number_a$csv.X7T_Hospitalisierung_Faelle,csv_only_number_a$csv.X7T_Hospitalisierung_Inzidenz,csv_only_number_a$csv.Bundesland_Id)
-  M <- cor(final_final_data)
+  M <- cor(data_corona)
   corrplot(M, method="color")
 }
 
+prediction <- function() {
+  fit <- lm(csv.X7T_Hospitalisierung_Faelle ~ as.Date(c(csv.Datum)), data=final_data)
+  new_data <- data.frame(csv.Datum = as.Date(c("2022-11-1","2022-11-30")))
+  predict(fit, new_data, interval = "confidence")
+}
+
 extrem_wert_analyse()
-final_final_data <- prepare_data()
+data_corona <- prepare_data()
 plote_korrelationsmatrix()
+prediction()
 
